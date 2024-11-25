a0a65e299b2d54d26364ef2464f8b48c960f791f
ram451992 <ram451992@yahoo.in>
2024-11-24 20:55:02 -0500

    fixing a minor issue in registration

--- app/index.js
+++ app/index.js
@@ -77,8 +77,7 @@ export default function LoginScreen() {
             </>}
             {showRegistration && <>
                 <Text style={styles.subtitle}>Welcome! Please fill below details to Register.</Text>
-                <RegistrationForm handleRegistration={handleRegistration} setShowRegistration={setShowRegistration} />
-            </>}
+                <RegistrationForm username={username} setUsername={setUsername}  setPassword={setPassword} handleRegistration={handleRegistration} setShowRegistration={setShowRegistration} />            </>}
             {/*<GoogleLoginButton onPress={handleGoogleLogin} />*/}
         </View>
     );
